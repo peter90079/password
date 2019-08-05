cd8f67063071e5be6b5c64e895aedfddf83c3af3
Peter Chen <peter90079@gmail.com>
2019-08-05 23:26:15 +0800

    3rd edit

--- password.py
+++ password.py
@@ -1,12 +1,13 @@
 x = 3
 while x > 0 :
+	x = x - 1
 	password = input('請輸入密碼')
 	if password == 'a123456':
 		print('登入成功')
 		break
 	else:
-		x = x - 1
-		print('密碼錯誤！你剩', x,'次機會')
-		if x == 0:
+		print('密碼錯誤')
+		if x > 0:
+			print('還有', x,'次機會')
+		else :
 			print('帳號已鎖住！請臨櫃辦理')
-			break
